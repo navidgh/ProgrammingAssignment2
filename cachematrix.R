45822c49c84f18ee5ee036a0e386f68fe470da56
Navid <navidgh68@gmail.com>
2015-06-13 22:17:10 -0700

    HW2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##  This function creates a list containing a function to
+##  get and set the value of a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) inv <<- solve
+    getinv <- function() inv
+    list(set = set, get = get,
+    setinv = setinv,
+    getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function finds the mean of the special matrix created with
+## above function. However, first checks the cache!
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
